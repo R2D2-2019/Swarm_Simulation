b409339a3485698c058e35fbb56faba2501f229f
Rick van Sambeek <rickvansambeek@gmail.com>
2019-05-28 13:45:07 +0200

    removed version conflict controll markers from repo

--- code/headers/robo_cop_c.hpp
+++ code/headers/robo_cop_c.hpp
@@ -7,14 +7,8 @@
 
 namespace r2d2::robo_cop {
 	class robo_cop_c {
-<<<<<<< Updated upstream
         public:
             robo_cop_c(int x);
             void move(int x);
-=======
-	public:
-		robo_cop_c();
-		void move(int x);
->>>>>>> Stashed changes
 	};
 }
